2c5fbd968866f891016ed2a78bf11cd2ddf5dd78
Tommy Carter <tommyjr@gmail.com>
2026-04-29 17:07:56 -0500

    Optimize deps, whatever that means

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,6 +8,23 @@ import { playwright } from '@vitest/browser-playwright';
 const isCI = process.env.CI === 'true';
 
 export default defineConfig({
+  optimizeDeps: {
+    include: [
+      // I'm not sure why I need to do this, but without it, I was getting test
+      // failures preceeded by this:
+      //    [vitest] Vite unexpectedly reloaded a test. This may cause tests to
+      //    fail, lead to flaky behaviour or duplicated test runs.
+      'ember-source/@ember/service/index.js',
+      '@embroider/router',
+      '@warp-drive/legacy',
+      '@warp-drive/json-api',
+      'ember-page-title',
+      '@warp-drive/ember',
+      'ember-source/@ember/routing/index.js',
+      '@warp-drive/core/reactive',
+      '@warp-drive/core/request',
+    ],
+  },
   test: {
     include: ['tests/**/*.test.{gjs,gts,js,ts}'],
     maxConcurrency: 1,
