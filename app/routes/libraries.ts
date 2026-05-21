28702d0d0cac6352ec60ca1a74dcfa29d18afed2
Tommy Carter <tommyjr@gmail.com>
2026-05-21 12:30:04 -0500

    Use parallel API calls

--- app/routes/libraries.ts
+++ app/routes/libraries.ts
@@ -2,9 +2,10 @@ import Route from '@ember/routing/route';
 
 export default class LibsRoute extends Route {
   async model() {
-    // TODO: do not await these serially
-    const libsModule = await import('../../data/chicago-libs.json');
-    const mapModule = await import('../../data/chicago-library-map-data.json');
+    const [libsModule, mapModule] = await Promise.all([
+      import('../../data/chicago-libs.json'),
+      import('../../data/chicago-library-map-data.json'),
+    ]);
     return { libraries: libsModule.default, map: mapModule.default };
   }
 }
