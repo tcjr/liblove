7c86e22dc1ca0637b8876489fa731edb8d515d92
Tom Carter <tommyjr@gmail.com>
2026-05-22 14:44:39 +0000

    Update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    // baseUrl: 'http://localhost:8888',
-    baseUrl: 'https://liblove.netlify.app',
+    baseUrl: 'http://localhost:8888',
+    // baseUrl: 'https://liblove.netlify.app',
 
     // setupNodeEvents(/*_on, _config*/) {
     //   // implement node event listeners here
