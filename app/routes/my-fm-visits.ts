47ed8ddecd3340f141c189044638e7500df9ac54
Tommy Carter <tommyjr@gmail.com>
2026-05-24 07:24:36 -0500

    Farmer's market map generation

--- app/routes/my-fm-visits.ts
+++ app/routes/my-fm-visits.ts
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default class MyFmVisitsRoute extends Route {
   async model() {
     const [fmModule, mapModule] = await Promise.all([
-      import('../../data/farmers-markets.json'),
+      import('../../data/chicago-farmers-markets.json'),
       import('../../data/chicago-fm-map-data.json'),
     ]);
     return { markets: fmModule.default, map: mapModule.default };
